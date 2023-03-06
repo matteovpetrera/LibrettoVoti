2a1ca97ed869a0d5591c719d8a26e9cc705be4bb
matteopetrera <matteopetrera@localhost>
2023-03-06 11:24:44 +0100

    fine lezione

--- src/main/java/it/polito/tdp/libretto/model/Libretto.java
+++ src/main/java/it/polito/tdp/libretto/model/Libretto.java
@@ -1,5 +1,62 @@
 package it.polito.tdp.libretto.model;
 
-public class Libretto {
+import java.util.LinkedList;
+import java.util.List;
 
+public class Libretto {
+	
+	private List<Voto> voti;
+
+	public Libretto() {
+		super();
+		this.voti = new LinkedList<Voto>();
+	}
+	/**
+	 * Aggiungi un nuovo voto al libretto
+	 *(per ora non fa nessun controllo)
+	 * @param v Voto da aggiungere
+	 * @return true
+	 */
+	
+	public boolean add(Voto v) {
+		
+		return this.voti.add(v); // delega
+	}
+	
+	public void stampa() {
+		for(Voto v: this.voti) {
+			System.out.println(v.toString());
+		}
+	}
+	/**
+	 * metodo generalizzato senza utilizzare costanti ( es. voto = 25)
+	 * 
+	 * @param valoreVoto
+	 */
+	public void stampaVotoParametro(int valoreVoto) {
+		for(Voto v: this.voti) {
+			if(v.getVotoOttenuto()==valoreVoto) {
+				System.out.println(v.toString());
+			}
+		}
+	}
+	
+	public Voto ricercaVotoPerNome(String corso) {
+		
+		for(Voto v: this.voti) {
+			if(v.getNomeCorso().equals(corso)) {
+				return v;
+			}
+		}
+		return null;
+		//throw new RuntimeException("Voto non trovato");
+	}
+	public boolean esisteVoto(Voto nuovo) {
+		for(Voto v: this.voti) {
+			if(v.getNomeCorso().equals(nuovo.getNomeCorso()) && v.getVotoOttenuto()==nuovo.getVotoOttenuto()) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
--- src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
@@ -1,10 +1,32 @@
 package it.polito.tdp.libretto.model;
 
+import java.time.LocalDate;
+
 public class TestLibretto {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Libretto lib = new Libretto();
+		
+		lib.add(new Voto("Analisi 1", 29, LocalDate.of(2021, 2, 15)));
+		lib.add(new Voto("Fisica 2", 21, LocalDate.of(2021, 3, 12)));
+		lib.add(new Voto("Informatica", 25, LocalDate.of(2022, 1, 21)));
+		//metodo stampa
+		lib.stampa();
+		System.out.println();
+		//lib.stampaVotoParametro(25);
+		Voto v = lib.ricercaVotoPerNome("Analisi 1");
+		System.out.println(v.toString());
+		
+		//
+		Voto a1bis = new Voto("Analisi 1", 29, LocalDate.of(2025, 2, 25));
+		Voto a1ter = new Voto("Analisi 1", 30, LocalDate.of(2025, 2, 25));
+		/*
+		 * il metodo compara sia il nome che il voto, quindi nel primo caso restituisce true
+		 * nel secondo caso no in quanto i voti sono diversi
+		 */
+		System.out.println(a1bis.toString()+" è duplicato? "+ lib.esisteVoto(a1bis));
+		System.out.println(a1ter.toString()+" è duplicato? "+ lib.esisteVoto(a1ter));
 	}
-
+	
 }
